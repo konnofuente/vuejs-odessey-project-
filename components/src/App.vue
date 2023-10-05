59714748916911e1c0b0f8ccaed90611d2d908d3
konnofuente <konomelifuente@gmail.com>
2023-10-05 03:43:15 +0100

    setting up project complete

---
+++ components/src/App.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <div>
+
+    <h3>texting</h3>
+    <button @click="age ++"> increase age</button>
+    <greeting-vue></greeting-vue>
+    <user-vue :age="age"></user-vue>
+  </div>
+</template>
+
+
+<script >
+import GreetingVue from './components/GreetingVue.vue';
+import UserVue from './components/UserVue.vue';
+export default {
+  name:'App',
+  components:{
+    GreetingVue,
+    UserVue
+  },
+  data() {
+    return {
+      age:26
+    }
+  },
+}
+
+</script>
---
+++ components/src/components/UserVue.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>The user have age {{ age }} years</p>
+    </div>
+</template>
+
+<script >
+export default {
+    name:"UserVue",
+    props:["age"]
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
