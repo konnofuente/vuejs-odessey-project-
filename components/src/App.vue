3c9747e3e8347709ee33dc37515da2936d3d29e0
konnofuente <konomelifuente@gmail.com>
2023-10-05 08:08:54 +0100

    slot implementation

--- components/src/App.vue
+++ components/src/App.vue
@@ -1,27 +1,61 @@
 
 <template>
-  <div>
+<div>
+  <form-vue>
+    <template   v-slot:help>
+        <p>This is a form</p>
+    </template>
 
-    <h3>texting</h3>
-    <button @click="age ++"> increase age</button>
-    <greeting-vue></greeting-vue>
-    <user-vue :age="age"></user-vue>
-  </div>
+    <template  v-slot:fields>
+        <input type="text" placeholder="name" name="" id="">
+        <input type="text" placeholder="email" name="" id="">
+        <input type="text" placeholder="tel" name="" id="">
+    </template>
+
+    <template  v-slot:buttons> 
+        <button type="submit">Submit</button>
+    </template>
+  </form-vue>
+
+  <form-vue>
+    <form action="">
+    <div class="help">
+        <p>This is a form</p>
+    </div>
+
+    <div class="fields">
+        <input type="text" placeholder="name" name="" id="">
+        <input type="text" placeholder="email" name="" id="">
+    </div>
+
+    <div class="buttons"> 
+        <button type="submit">Submit</button>
+    </div>
+   </form>
+  </form-vue>
+</div>
 </template>
 
 
 <script >
-import GreetingVue from './components/GreetingVue.vue';
-import UserVue from './components/UserVue.vue';
+// import GreetingVue from './components/GreetingVue.vue';
+// import UserVue from './components/UserVue.vue';
+import FormVue from './components/FormVue.vue';
 export default {
   name:'App',
   components:{
-    GreetingVue,
-    UserVue
+    // GreetingVue,
+    // UserVue,
+    FormVue
   },
   data() {
     return {
       age:26
+    }
+  },
+  methods: {
+    onclickAge(){
+      return this.age ++;
     }
   },
 }
---
+++ components/src/components/FormVue.vue
@@ -0,0 +1,21 @@
+<template>
+   <form action="">
+    <div>
+        <slot name="help"></slot>
+    </div>
+    <div>
+        <slot name="fields"></slot>
+    </div>
+    <div>
+        <slot name="buttons"></slot>
+    </div>
+   </form>
+</template>
+
+<script >
+
+export default {
+    name: "FormVue",
+}
+
+</script>
