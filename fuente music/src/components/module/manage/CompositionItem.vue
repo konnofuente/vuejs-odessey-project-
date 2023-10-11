2521ff33dc690366352875cba498eab7e883efdb
konnofuente <konomelifuente@gmail.com>
2023-10-11 14:27:11 +0100

    listing song information

---
+++ fuente music/src/components/module/manage/CompositionItem.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <div class="border border-gray-200 p-3 mb-4 rounded">
+          <div v-show="!showForm">
+            <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
+              <button 
+              
+              class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right">
+                <i class="fa fa-times"></i>
+              </button>
+              <button 
+              @click="showForm = !showForm"
+              class="ml-1 py-1 px-2 text-sm rounded text-white bg-blue-600 float-right">
+                <i class="fa fa-pencil-alt"></i>
+              </button>
+          </div>
+
+          <div v-show="showForm">
+            <form>
+              <div class="mb-3">
+                <label class="inline-block mb-2">Song Title</label>
+                <input
+                  type="text"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Enter Song Title"
+                />
+              </div>
+              <div class="mb-3">
+                <label class="inline-block mb-2">Genre</label>
+                <input
+                  type="text"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Enter Genre"
+                />
+              </div>
+              <button 
+              @click.prevent="showForm = !showForm"
+              type="submit" class="py-1.5 px-3 rounded text-white bg-green-600">
+                Submit
+              </button>
+              <button type="button" class="py-1.5 px-3 rounded text-white bg-gray-600">
+                Go Back
+              </button>
+            </form>
+          </div>
+
+
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.bg-green-600{
+  background-color:rgb(83, 160, 83);
+}
+
+.bg-gray-600 {
+  background-color: gray;
+}
+</style>
+<script>
+
+
+
+
+export default {
+  name: 'CompositionItem',
+  data(){
+    return{
+      showForm:false
+    }
+  },
+  props: {
+    song:{
+      type: Object,
+      required: true,
+    }
+  }
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
