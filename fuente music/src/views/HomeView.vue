f3940f3523e4f6d7f454abffbd44498328c5da99
konnofuente <konomelifuente@gmail.com>
2023-10-17 04:51:51 +0100

    update: updated css error

--- fuente music/src/views/HomeView.vue
+++ fuente music/src/views/HomeView.vue
@@ -2,17 +2,13 @@
   <div>
     <!-- Introduction -->
     <section
-      style="background-image: url(../public/assets/img/header.png)"
-      class="mb-8 py-20 text-black text-center relative"
+      class="mb-8 py-20 text-black text-center relative landing-bg"
     >
       <div class="absolute inset-0 w-full h-full bg-contain introduction-bg"></div>
       <div class="container mx-auto">
         <div class="text-white main-header-content">
-          <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
-          <p class="w-full md:w-8/12 mx-auto">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor mollis,
-            congue augue non, venenatis elit. Nunc justo eros, suscipit ac aliquet imperdiet,
-            venenatis et sapien. Duis sed magna pulvinar, fringilla lorem eget, ullamcorper urna.
+          <h1 class="font-bold text-5xl mb-5">Fuente Music: Unleash Your Melody!</h1>
+          <p class="w-full md:w-8/12 mx-auto">Where Artists and Music Lovers Connect. Upload, Listen, and Discover Your Next Favorite Song.
           </p>
         </div>
       </div>
